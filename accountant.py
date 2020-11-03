6a5473ef646c613fd30bcf4ae8cef268f2fb2374
Lisukod <45397160+Lisukod@users.noreply.github.com>
2020-11-03 14:21:53 +0100

    Dodanie funkcji sale_fun

--- accountant.py
+++ accountant.py
@@ -16,14 +16,27 @@ def buy_fun():
     product_amount = int(input())
     if saldo - unit_price*product_amount < 0:
         print("Błąd. Ujemne saldo po zakupie {} w ilości {}".format(product_id, product_amount))
-        return
+        return False
     elif product_amount < 0:
         print("Błąd. Ujemna ilość zakupionego towaru {} w ilości {}".format(product_id, product_amount))
-        return
+        return False
     elif unit_price*product_amount < 0:
         print("Błąd. Ujemna kwota zakupu {} w ilości {}".format(product_id, product_amount))
-        return
+        return False
     saldo -= unit_price*product_amount
+
+def sale_fun():
+    global saldo
+    product_id = input()
+    unit_price = int(input())
+    product_amount = int(input())
+    if product_amount < 0:
+        print("Błąd. Ujemna ilość zakupionego towaru {} w ilości {}".format(product_id, product_amount))
+        return False
+    elif unit_price*product_amount < 0:
+        print("Błąd. Ujemna kwota zakupu {} w ilości {}".format(product_id, product_amount))
+        return False
+    saldo += unit_price*product_amount
 
 switchCase = {
     "saldo": saldo_fun(),
@@ -38,7 +51,9 @@ print("Wybierz jedną z poniższych akcji.\nsaldo | zakup | sprzedaż")
 while True:
     action = input()
     if action == "saldo" or "zakup" or "sprzedaż":
-        switchCase[action]
+            check = switchCase[action]
+            if not check:
+                break
     elif action == "stop":
         break
     else:
