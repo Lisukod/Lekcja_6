573cc94b4bcda569bbf201e3fb598dd8d0c8012e
Lisukod <45397160+Lisukod@users.noreply.github.com>
2020-11-03 13:57:41 +0100

    Dodanie plików przykadowych | Dodanie funkcji saldo_fun i buy_fun

--- accountant.py
+++ accountant.py
@@ -1,10 +1,45 @@
 import sys
 
+saldo = 0
+logs = []
+
+def saldo_fun():
+    global saldo
+    saldo += int(input())
+    comment = input()
+    print("{}: {}".format(comment, saldo))
+
+def buy_fun():
+    global saldo
+    product_id = input()
+    unit_price = int(input())
+    product_amount = int(input())
+    if saldo - unit_price*product_amount < 0:
+        print("Błąd. Ujemne saldo po zakupie {} w ilości {}".format(product_id, product_amount))
+        return
+    elif product_amount < 0:
+        print("Błąd. Ujemna ilość zakupionego towaru {} w ilości {}".format(product_id, product_amount))
+        return
+    elif unit_price*product_amount < 0:
+        print("Błąd. Ujemna kwota zakupu {} w ilości {}".format(product_id, product_amount))
+        return
+    saldo -= unit_price*product_amount
+
 switchCase = {
-    "saldo": "1",
-    "sprzedaż": "2",
-    "zakup": "3",
+    "saldo": saldo_fun(),
+    "sprzedaż": sale_fun(),
+    "zakup": buy_fun(),
     "konto": "4",
     "magazyn": "5",
     "przegląd": "6"
 }
+
+print("Wybierz jedną z poniższych akcji.\nsaldo | zakup | sprzedaż")
+while True:
+    action = input()
+    if action == "saldo" or "zakup" or "sprzedaż":
+        switchCase[action]
+    elif action == "stop":
+        break
+    else:
+        print("Błędna nazwa operacji")
