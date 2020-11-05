01d53e6b57d77268f6c5611f11be36bc3492b856
Lisukod <45397160+Lisukod@users.noreply.github.com>
2020-11-05 15:24:52 +0100

    Wersja do wysłania

--- accountant.py
+++ accountant.py
@@ -2,22 +2,16 @@ from sys import argv
 
 saldo = 0
 logs = []
-storehouse = []
+storehouse = {}
 check = True
 
-def saldo_fun():
+def saldo_fun(temp_saldo, comment):
     global saldo
-    temp_saldo = int(input())
     saldo += temp_saldo
-    comment = input()
-    logs.append((comment, saldo))
-    # print("{}: {}".format(comment, saldo))
-
-def buy_fun():
+    logs.append(("saldo", temp_saldo, comment))
+    
+def buy_fun(product_id, unit_price, product_amount):
     global saldo, check
-    product_id = input()
-    unit_price = int(input())
-    product_amount = int(input())
     if saldo - unit_price*product_amount < 0:
         print("Błąd. Ujemne saldo po zakupie {} w ilości {}".format(
             product_id, product_amount))
@@ -33,19 +27,15 @@ def buy_fun():
             product_id, product_amount))
         check = False
         return
-    logs.append((product_id, unit_price, product_amount))
+    logs.append(("zakup", product_id, unit_price, product_amount))
     if product_id in storehouse:
-        storehouse[storehouse.index(product_id)+1] += product_amount
+        storehouse[product_id] += product_amount
     else:
-        storehouse.append(product_id)
-        storehouse.append(product_amount)
+        storehouse[product_id] = product_amount
     saldo -= unit_price*product_amount
 
-def sale_fun():
+def sale_fun(product_id, unit_price, product_amount):
     global saldo, check
-    product_id = input()
-    unit_price = int(input())
-    product_amount = int(input())
     if product_amount < 0:
         print("Błąd. Ujemna ilość zakupionego towaru {} w ilości {}".format(
             product_id, product_amount))
@@ -56,35 +46,77 @@ def sale_fun():
             product_id, product_amount))
         check = False
         return
-    logs.append((product_id, unit_price, product_amount))
+    logs.append(("sprzedaz", product_id, unit_price, product_amount))
     if product_id in storehouse:
-        storehouse[storehouse.index(product_id)+1] -= product_amount
+        storehouse[product_id] -= product_amount
     else:
-        print("Błąd. Brak takiego {} produktu na magazynie".format(product_id))
+        print("Błąd. Brak produktu {} na magazynie".format(product_id))
         check = False
         return 
     saldo += unit_price*product_amount
 
+def printOut(logs):
+    for log in logs:
+        if log == "stop":
+            print("{}".format(log))
+        else:
+            for log_element in log:
+                print("{}".format(log_element))
+
 switchCase = {
-    "saldo": saldo_fun,
-    "sprzedaż": sale_fun,
-    "zakup": buy_fun,
-    # "konto": konto_fun()
+    "sprzedaz": sale_fun,
+    "zakup": buy_fun
 }
 
-print("Wybierz jedną z poniższych akcji.\nsaldo | zakup | sprzedaż")
 while check:
-    action = input()
-    if action == "saldo" or  action == "zakup" or action == "sprzedaż":
-        switchCase[action]()
+    action = input().strip()
+    if action == "saldo":
+        x = int(input())
+        y = input()
+        saldo_fun(x, y)
+    elif action == "zakup" or action == "sprzedaz":
+        x = input()
+        y = int(input()) 
+        z = int(input())
+        switchCase[action](x, y, z)
     elif action == "stop":
+        logs.append("stop")
         break
     else:
-        print("Błędna nazwa operacji")
+        print("Błędna nazwa operacji. Podano {}".format(action))
+        break
 
-# if check:
-#     print(saldo)
-
-# for index, log in enumerate(logs):
-#     print("{}. {}".format(index, log))
-
+if len(argv) == 1:
+    printOut(logs)
+elif argv[1] == "saldo":
+    saldo_fun(int(argv[2]), argv[3])
+    print(saldo)
+elif argv[1] == "zakup":
+    logs.pop()
+    buy_fun(argv[2], int(argv[3]), int(argv[4]))
+    logs.append("stop")
+    printOut(logs)
+elif argv[1] == "sprzedaz":
+    logs.pop()
+    sale_fun(argv[2], int(argv[3]), int(argv[4]))
+    logs.append("stop")
+    printOut(logs)
+elif argv[1] == "konto":
+    print(saldo)
+elif argv[1] == "magazyn":
+    for name in argv[2:]:
+        if name in storehouse:
+            print("{}: {}".format(name, storehouse[name]))
+        else:
+            print("{}: 0".format(name))
+elif argv[1] == "przeglad":
+    for index, log in enumerate(logs):
+        if index >= int(argv[2]) and index <= int(argv[3]):
+            if log == "stop":
+                print("{}".format(log))
+                break
+            else:
+                for log_element in log:
+                    print("{}".format(log_element))
+else:
+    printOut(logs)
