da6423339efc98526e64c311cfd08c5bc84e4cbc
Lisukod <45397160+Lisukod@users.noreply.github.com>
2020-11-05 16:37:57 +0100

    Przeniesienie całego programu do pętli While | Poprawienie wyświetlanych wyników

--- accountant.py
+++ accountant.py
@@ -80,43 +80,42 @@ while check:
         z = int(input())
         switchCase[action](x, y, z)
     elif action == "stop":
+        if len(argv) == 1:
+            printOut(logs)
+        elif argv[1] == "saldo":
+            saldo_fun(int(argv[2]), argv[3])
+            print(saldo)
+            break
+        elif argv[1] == "zakup":
+            buy_fun(argv[2], int(argv[3]), int(argv[4]))
+            printOut(logs)
+        elif argv[1] == "sprzedaz":
+            sale_fun(argv[2], int(argv[3]), int(argv[4]))
+            printOut(logs)
+        elif argv[1] == "konto":
+            print(saldo)
+            break
+        elif argv[1] == "magazyn":
+            for name in argv[2:]:
+                if name in storehouse:
+                    print("{}: {}".format(name, storehouse[name]))
+                else:
+                    print("{}: 0".format(name))
+        elif argv[1] == "przeglad":
+            for index, log in enumerate(logs):
+                if index >= int(argv[2]) and index <= int(argv[3]):
+                    if log == "stop":
+                        print("{}".format(log))
+                        break
+                    else:
+                        for log_element in log:
+                            print("{}".format(log_element))
+        else:
+            printOut(logs)
+
         logs.append("stop")
+        print(logs[-1])
         break
     else:
         print("Błędna nazwa operacji. Podano {}".format(action))
         break
-
-if len(argv) == 1:
-    printOut(logs)
-elif argv[1] == "saldo":
-    saldo_fun(int(argv[2]), argv[3])
-    print(saldo)
-elif argv[1] == "zakup":
-    logs.pop()
-    buy_fun(argv[2], int(argv[3]), int(argv[4]))
-    logs.append("stop")
-    printOut(logs)
-elif argv[1] == "sprzedaz":
-    logs.pop()
-    sale_fun(argv[2], int(argv[3]), int(argv[4]))
-    logs.append("stop")
-    printOut(logs)
-elif argv[1] == "konto":
-    print(saldo)
-elif argv[1] == "magazyn":
-    for name in argv[2:]:
-        if name in storehouse:
-            print("{}: {}".format(name, storehouse[name]))
-        else:
-            print("{}: 0".format(name))
-elif argv[1] == "przeglad":
-    for index, log in enumerate(logs):
-        if index >= int(argv[2]) and index <= int(argv[3]):
-            if log == "stop":
-                print("{}".format(log))
-                break
-            else:
-                for log_element in log:
-                    print("{}".format(log_element))
-else:
-    printOut(logs)
